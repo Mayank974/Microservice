b2cd14d97dc232f33bf1f86dc7bfe5c269659e04
Mayank Tiwari <tiwari12mayank123@gmail.com>
2024-12-01 23:30:51 +0530

    Accounts Microservice:UPDATE: Api Documentation

--- accounts/src/main/java/com/mn/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/mn/accounts/AccountsApplication.java
@@ -4,8 +4,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Mayank Tiwari",
+						email = "test@gmail.com",
+						url = "https://www.mnsoft.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.mnsoft.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- accounts/src/main/java/com/mn/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/mn/accounts/dto/AccountsDto.java
@@ -1,21 +1,33 @@
 package com.mn.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
 	@Min(value = 1_000_000_000L, message = "Account number should be 10 digits")
 	@Max(value= 9_999_999_999L, message = "Account number should be 10 digits")
+	@Schema(
+	            description = "Account Number of Eazy Bank account", example = "3454433243"
+    )
 	private Long accountNumber;
-
 	@NotEmpty(message = "AccountType can not be a null or empty")
-	private String accountType;
+    @Schema(
+            description = "Account type of Eazy Bank account", example = "Savings"
+    )
+    private String accountType;
 
-	@NotEmpty(message = "BranchAddress can not be a null or empty")
-	private String branchAddress;
-
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
+    @Schema(
+            description = "Eazy Bank branch address", example = "123 NewYork"
+    )
+    private String branchAddress;
 }
